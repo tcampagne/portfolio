c2058a9e817cba665e10628ff9a0d4e15bd49089
tcampagne <thibault.campagne@gmail.com>
2024-06-20 12:41:55 +0200

    test without animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SidebarComponent} from "./components/sidebar/sidebar.component";
 import {AboutComponent} from "./components/about/about.component";
 import {HomeComponent} from "./components/home/home.component";
+import {NgIf, NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, AboutComponent, HomeComponent],
+  imports: [RouterOutlet, SidebarComponent, AboutComponent, HomeComponent, NgStyle, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
